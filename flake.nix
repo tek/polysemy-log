b2bc12f23e0df2f1cf5d8781126c6ebc0e5e5da3
Torsten Schmits <torstenschmits@gmail.com>
2021-04-06 21:49:59 +0200

    bump deps

--- flake.nix
+++ flake.nix
@@ -15,19 +15,16 @@
       url = github:nitros12/di-polysemy;
       flake = false;
     };
-    conc.url = github:tek/polysemy-conc;
-    polysemy-time.url = github:tek/polysemy-time;
-    polysemy-test.follows = "conc/polysemy-test";
+    polysemy-conc.url = github:tek/polysemy-conc;
+    polysemy-time.follows = "polysemy-conc/polysemy-time";
+    polysemy-test.follows = "polysemy-conc/polysemy-test";
   };
 
-  outputs = { tryp-hs, polysemy-time, polysemy-test, conc, ... }@inputs:
+  outputs = { tryp-hs, polysemy-time, polysemy-test, polysemy-conc, ... }@inputs:
   let
     common = { hackage, source, jailbreak, ... }: {
       polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
       polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
-      polysemy-time = source.sub polysemy-time "packages/time";
-      polysemy-chronos = source.sub polysemy-time "packages/chronos";
-      polysemy-conc = source.sub conc "packages/conc";
       co-log = jailbreak (hackage "0.4.0.1" "05f37lq1kwlmm62n1n932l8jnqirmlf87var2n2zb0cslmv63yxg");
       co-log-polysemy = jailbreak (hackage "0.0.1.2" "17bcs8dvrhwfcyklknkqg11gxgxm2jaa7kbm6xx4vm1976abzwss");
       di-polysemy = hackage "0.2.0.0" "1c6c4qx6ljx1ac10qic1fhrj282cs7cdx2q28lr5xhk73r5vabvf";
@@ -39,10 +36,16 @@
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       polysemy-test = source.sub polysemy-test "packages/polysemy-test";
+      polysemy-time = source.sub polysemy-time "packages/time";
+      polysemy-chronos = source.sub polysemy-time "packages/chronos";
+      polysemy-conc = source.sub polysemy-conc "packages/conc";
     };
 
     compatOverrides = { hackage, source, only, ... }@args: common args // {
       polysemy-test = hackage "0.3.1.1" "0x0zg1kljr7a1mwmm3zrmha5inz3l2pkldnq65fvsig8f3x8rsar";
+      polysemy-time = hackage "0.1.2.1" "09l8r5fx0vnapdn8p0cwiwprgg3i67m58dd4j5hcdhw34gfqnnsr";
+      polysemy-chronos = hackage "0.1.2.1" "0layan6jxg857n2dmxwnylichnk2ynlpxih5iya3q8x2nbndpbl2";
+      polysemy-conc = hackage "0.1.0.2" "0ijz5l8q53d1s7i100gvjdhzv80dpd140m7a9hyam113ybglc8lg";
     };
   in
   tryp-hs.flake {
