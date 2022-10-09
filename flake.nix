f29881fca591d1be958112e20ac4713f2d282e9b
Torsten Schmits <git@tryp.io>
2022-10-10 01:17:47 +0200

    bump hix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Polysemy Effects for Logging";
 
   inputs = {
-    hix.url = github:tek/hix;
-    polysemy-conc.url = github:tek/polysemy-conc;
+    hix.url = git+https://git.tryp.io/tek/hix;
+    polysemy-conc.url = git+https://git.tryp.io/tek/polysemy-conc?tag=v0.9.0.0;
   };
 
   outputs = { hix, polysemy-conc, ... }:
@@ -12,34 +12,27 @@
     ghc902 = { hackage, ... }: {
       polysemy = hackage "1.7.1.0" "0qwli1kx3hk68hqsgw65mk81bx0djw1wlk17v8ggym7mf3lailyc";
       polysemy-plugin = hackage "0.4.3.0" "1r7j1ffsd6z2q2fgpg78brl2gb0dg8r5ywfiwdrsjd2fxkinjcg1";
-      typerep-map = hackage "0.5.0.0" "1j2jbl8qg88zz5yydfi3iqrqmazch038ra7cpccylxz1rdyqm9k3";
     };
 
-    ghc922 = { hackage, jailbreak, notest, ... }: {
-      chronos = jailbreak;
+    ghc924 = { hackage, jailbreak, notest, ... }: {
       polysemy = hackage "1.7.1.0" "0qwli1kx3hk68hqsgw65mk81bx0djw1wlk17v8ggym7mf3lailyc";
       polysemy-plugin = hackage "0.4.3.0" "1r7j1ffsd6z2q2fgpg78brl2gb0dg8r5ywfiwdrsjd2fxkinjcg1";
       type-errors = notest;
-      typerep-map = hackage "0.5.0.0" "1j2jbl8qg88zz5yydfi3iqrqmazch038ra7cpccylxz1rdyqm9k3";
     };
 
     all = { hackage, source, unbreak, jailbreak, ... }: {
-      co-log = jailbreak;
-      co-log-core = jailbreak;
-      co-log-polysemy = jailbreak unbreak;
-      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
-      polysemy-conc = hackage "0.9.0.0" "0yfbgbw58inl4q17qagdrx7pl3h258nna8w8j5nyjdqdrh25as6y";
+      co-log-polysemy = jailbreak;
     };
 
-  in hix.lib.flake ({ config, lib, ... }: {
-    base = ./.;
+  in hix.lib.pro ({ config, lib, ... }: {
     packages = {
       polysemy-log = ./packages/polysemy-log;
       polysemy-log-co = ./packages/polysemy-log-co;
       polysemy-log-di = ./packages/polysemy-log-di;
     };
     main = "polysemy-log";
-    overrides = { inherit all ghc902 ghc922; };
+    devGhc.compiler = "ghc902";
+    overrides = { inherit all ghc902 ghc924; };
     deps = [polysemy-conc];
     hpack.packages = import ./ops/hpack.nix { inherit config lib; };
     hackage.versionFile = "ops/version.nix";
