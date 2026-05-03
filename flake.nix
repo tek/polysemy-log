4b7ad5703c35d72ceb40a553fff948ac401a7692
Torsten Schmits <git@tryp.io>
2026-05-04 00:13:09 +0200

    Remove `versionFile`

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 
   outputs = { hix, ... }: hix.lib.pro ({config, ...}: {
     ghcVersions = ["ghc98" "ghc910" "ghc912"];
-    release.versionFile = "ops/version.nix";
     main = "polysemy-log";
     gen-overrides.enable = true;
 
